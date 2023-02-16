4e7124fe47ddcdfe02b2f997a294c427adcdddc7
Mateus da Costa Leme <101108090+MateusLem@users.noreply.github.com>
2023-02-16 18:06:38 -0300

    Add files via upload

---
+++ Desafio Aula 1-2/ConsoleApp1/Components/Mechanics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.components
+{
+    internal class Mechanics
+    {
+        private double accel { get; set; }
+        private double velc { get; set; }
+        private double mass { get; set; }
+
+        private double position { get; set; }
+
+        private double time { get; set; }
+
+        public void CacularForca(double mass, double accel)
+        {
+            double forca = accel * mass;
+            Console.WriteLine($"Massa: {mass}\nAceleração: {accel}\nForça Resultante: {forca}");
+        }
+
+        public void CalcularVelocidade(double position, double time)
+        {
+            double velocidade = position / time;
+            Console.WriteLine($"Deslocamento: {position}\nVariação do Tempo: {time}\nVelocidade Média: {velocidade}");
+        }
+
+        
+
+    }
+}
---
+++ Desafio Aula 1-2/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using ConsoleApp1.components;
+
+Mechanics mec = new();
+
+mec.CacularForca(50, 3);
+
+mec.CalcularVelocidade(50, 3);
+
+
